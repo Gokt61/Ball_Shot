0ccf00ee49df243176af75bdf08bf30dcade0137
Gokt61 <49290553+Gokt61@users.noreply.github.com>
2023-05-26 01:15:52 +0300

    Platfromlara anismayon ve etkileşim özellikleri eklendi

---
+++ BallShoot/Assets/SilindirYonet.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SilindirYonet : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    bool ButtonPressed;
+    public GameObject SilindirObjesi;
+    [SerializeField] private float DonusCapi;
+    [SerializeField] private string Yon;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("BASIYORUM");
+        ButtonPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("BIRAKTIM");
+        ButtonPressed = false;
+    }
+
+    void Update()
+    {
+        if (ButtonPressed) 
+        {
+            if (Yon =="Sol")
+            {
+                SilindirObjesi.transform.Rotate(0, DonusCapi * Time.deltaTime, 0, Space.Self);
+            }
+            else
+            {
+                SilindirObjesi.transform.Rotate(0, -DonusCapi * Time.deltaTime, 0, Space.Self);
+            }
+        }        
+    }
+}
